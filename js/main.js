62a8727c722d9a476740ca1ef16fa3234e3c2fca
daffodilho <46694934+daffodilho@users.noreply.github.com>
2019-07-23 16:31:25 -0400

    array ref

---
+++ js/main.js
@@ -0,0 +1,37 @@
+(() => {
+	console.log('fired!')
+
+	let textContainer = document.querySelector(".text-container"),
+		textButton = document.querySelector("button"),
+		bioButtons = document.querySelectorAll(".bio-buttons button");
+
+	const dataContainer = ["This ", "is ", "some ", "text! "];
+	const newContainer = ["This", "is", "a", "join", "method", "result"];
+	const billAndTed = ["I am Bill S Preston, Esq", "And I am Ted Theodore Logan. Party on, dude!"];
+
+	function showBioInfo() {
+		arrayIndex = this.dataset.arrayref;
+		textContainer.textContent = billAndTed[arrayIndex];
+	}
+
+	function joinArray() {
+		textContainer.textContent = newContainer.join(" ");
+	}
+
+	dataContainer.forEach((word, index) => {
+		textContainer.textContent += word;
+		console.log(index);
+	});
+
+	for (let i=0; i<dataContainer.length; i++) {
+		console.log(i, dataContainer[i]);
+
+		// append the array contents to the paragraph tag
+		textContainer.textContent += dataContainer[i];
+	}
+
+	textContainer.textContent += "I just added this with script!";
+	textButton.addEventListener("click", joinArray);
+	
+	bioButtons.forEach(button => button.addEventListener("click", showBioInfo));
+})();
